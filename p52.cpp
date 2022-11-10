5c9743326101ca63ff5711ce6a913627589ba0af
tina <83180446+tina2004@users.noreply.github.com>
2022-11-10 14:49:04 +0330

    Add files via upload

---
+++ p52.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+ using namespace std;
+  main() {
+   char English;
+    cout <<"Please enter an english word: ";
+    cin >> English;
+    switch  (English) {
+    	case 'a':
+    	 cout << "VOWEL";
+    	break;
+		case 'e':
+		 cout<< "VOWEL";
+		break; 
+		case 'i':
+		 cout << "VOWEL";
+		break;
+		case 'o':
+		 cout << "VOWEL";
+		break;
+		case 'u':
+		 cout << "VOWEL";
+		break;
+		case 'y':
+		 cout << "VOWEL";
+		break;   	
+	}
+}
